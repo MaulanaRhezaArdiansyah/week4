f15e792aed4f438af2e078ea7e200551c4bfcec2
Maulana Rheza <103730296+MaulanaRhezaArdiansyah@users.noreply.github.com>
2022-12-27 09:59:38 +0800

    menambah fitur at 27 des 2022

--- task2,3,4-new/main.js
+++ task2,3,4-new/main.js
@@ -1,32 +1,3 @@
-// form.addEventListener("submit", function (el) {
-//   el.preventDefault();
-
-//   let name = document.getElementById("name").value;
-//   let post = document.getElementById("post").value;
-
-//   //   fetch
-//   fetch("https://63a3f9ae9704d18da09a463d.mockapi.io/posts", {
-//     method: "POST",
-//     body: JSON.stringify({
-//       name: name,
-//       post: post,
-//     }),
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   })
-//     .then((response) => response.json())
-//     .then((json) => {
-//       console.log(json);
-//       let results = document.getElementById("results");
-
-//       results.innerHTML = `
-//         <p>${json.name}</p>
-//         <p>${json.post}</p>
-//         `;
-//     });
-// });
-
 // DECLARE VARIABLE
 
 const form = document.getElementById("form");
@@ -34,7 +5,7 @@ const addBtn = document.getElementById("add");
 const deleteBtn = document.getElementById("delete");
 
 // POST
-const addPost = () => {
+const addPost = (url) => {
   addBtn.addEventListener("click", async (el) => {
     el.preventDefault();
 
@@ -42,7 +13,7 @@ const addPost = () => {
     let post = document.getElementById("post").value;
 
     //   fetch
-    await fetch("https://63a3f9ae9704d18da09a463d.mockapi.io/posts", {
+    await fetch(url, {
       method: "POST",
       body: JSON.stringify({
         name: name,
@@ -55,22 +26,27 @@ const addPost = () => {
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        let results = document.getElementById("results");
+        // let results = document.querySelector(".results");
 
-        results.innerHTML = `
-    <p>${json.name}</p>
-    <p>${json.post}</p>
-    `;
+        //     results.innerHTML = `
+        // <p>${json.name}</p>
+        // <p>${json.post}</p>
+        // `
+        let conResult = document.getElementById("container-results");
+        conResult.innerHTML += `<div class="results">
+          <p>${json.name}</p>
+          <p>${json.post}</p>
+        </div>`;
       });
     getPost();
   });
 };
 
 // DELETE
-const deletePost = (id) => {
+const deletePost = (url) => {
   deleteBtn.addEventListener("click", async (el) => {
     el.preventDefault();
-    await fetch(`https://63a3f9ae9704d18da09a463d.mockapi.io/posts/1`, {
+    await fetch(url, {
       method: "DELETE",
     }).then(() => console.log("Delete post successfull"));
     getPost();
@@ -79,17 +55,7 @@ const deletePost = (id) => {
 
 // GET
 const getPost = () => {
-  fetch("https://63a3f9ae9704d18da09a463d.mockapi.io/posts/")
+  fetch("https://63a3f9ae9704d18da09a463d.mockapi.io/posts")
     .then((res) => res.json())
     .then((json) => console.log(json));
 };
-
-// DISPLAY DATA
-
-const displayData = () => {
-  addPost();
-  deletePost();
-  getPost();
-};
-
-displayData();
